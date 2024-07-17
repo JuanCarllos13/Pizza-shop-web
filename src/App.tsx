fca3eb0e4334ebcc8da61c25a06ab21d086fc129
Juan Carlos <juan2017amaral@gmail.com>
2024-07-16 20:36:53 -0400

    feat: eslint and prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import './global.css'
+
+import React from 'react'
 
 export function App() {
   return (
-    <>
+    <div className="flex h-screen items-center justify-center">
       <h1>hello word</h1>
-    </>
+    </div>
   )
 }
