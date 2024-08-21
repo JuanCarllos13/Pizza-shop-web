3decfdfaab0b02062e405d4614ad857449849991
Juan Carlos <juan2017amaral@gmail.com>
2024-08-21 19:28:31 -0400

    feat: create componente header

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -3,8 +3,9 @@ import { Outlet } from 'react-router-dom'
 export function AppLayout() {
   return (
     <div>
-      <h1>Autenticação</h1>
-      <div>
+      <h1 className="flex min-h-screen flex-col antialiased">Autenticação</h1>
+
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
         <Outlet />
       </div>
     </div>
